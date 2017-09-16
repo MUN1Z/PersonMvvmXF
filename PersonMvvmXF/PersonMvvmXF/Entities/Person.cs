1aebc5ea6f748e9c82109950c2da524edb4ebed8
Felipe Muniz <felipemunizicm2012@hotmail.com>
2017-09-16 14:01:57 -0300

    Adicionado o refit ao projeto e criado a entidade Pessoa

---
+++ PersonMvvmXF/PersonMvvmXF/Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace PersonMvvmXF.Entities
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public string Region { get; set; }
+        public int Age { get; set; }
+        public string Title { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Photo { get; set; }
+    }
+}
